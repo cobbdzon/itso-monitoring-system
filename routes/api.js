a8c3c120c01001d154f3d935f1422d9590f8ad56
cobbdzon <clarkjacob07@gmail.com>
2025-02-26 10:14:43 +0800

    logout functionality

--- routes/api.js
+++ routes/api.js
@@ -36,4 +36,11 @@ router.post(
 	})
 );
 
+router.post("/logout", (req, res, next) => {
+	req.logout((err) => {
+		if (err) { return next(err) }
+		res.redirect("/login")
+	})
+});
+
 export default router;
